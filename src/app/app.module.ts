f3bd3fee94ecad0f6cb898d257176fd146203340
vedant <vedantkarpe1@gmail.com>
2020-06-07 16:57:35 +0530

    updated introduction feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
   ], exports: [
-    MatCardModule
+    MatCardModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class AngularMaterialModule { }
