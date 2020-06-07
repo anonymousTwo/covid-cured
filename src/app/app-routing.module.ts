8aea3d41fd7f583ff861ab6a08cf82fd8b2bffc5
Pavan Shukla <pavan.shukla2008@gmail.com>
2020-06-07 14:12:54 +0530

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./modules/list/list.module').then(m => m.ListModule)
+  },
+  {
+    path: '*',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
+  declarations: [WelcomeComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
