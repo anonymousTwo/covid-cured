70a16163fe50b7f717f47d79320ffa392e4ace62
Nikhil Naik <nikhilnaik1988@gmail.com>
2020-06-08 18:45:33 +0530

    Added css as per the designs

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -13,16 +13,16 @@ export class WelcomeComponent implements OnInit {
 
   introductionContent: Section[] = [
     {
-      item: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
-      standard dummy text ever since the 1500s.`,
+      item: `Liability: As an entrepreneur, in principle you can be held liable for all direct and indirect damage. 
+      Although you may never rule out your liability entirely, you can limit it via your terms and conditions.`,
     },
     {
-      item: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
-      standard dummy text ever since the 1500s.`,
+      item: `Payment term: The legal payment term for invoices is 30 days. 
+      If you want a shorter payment term for your invoices, you will need to have explicitly agreed this via your general terms and conditions or in a contract.`,
     },
     {
-      item: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
-      standard dummy text ever since the 1500s.`,
+      item: `Retention of title: By including retention of title, you can ensure that as an entrepreneur you remain the owner of the item you deliver.This means that as owner, you have the right to reclaim the item as long as the invoice has not been paid. 
+      This also is the case if your counterparty goes bankrupt or into liquidation.`,
     }
   ];
 
